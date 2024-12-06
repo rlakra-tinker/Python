768a645b585e6c7245567ad12b1e05be36f6b41b
Rohtash <rohtash_lakra@tinkerdiy.com>
2024-12-05 22:42:46 -0800

    added list and set build-in tests

--- core/collection/list.py
+++ core/collection/list.py
@@ -1,7 +1,17 @@
 #
 # Author: Rohtash Lakra
 #
+from collections.abc import Container
+
 from core.collection.queue import Queue
+
+
+class List(Container):
+
+    def __add__(self, other):
+        pass
+
+    pass
 
 
 class LinkedList(Queue):
---
+++ tests/core/collection/test_list.py
@@ -0,0 +1,14 @@
+#
+# Author: Rohtash Lakra
+#
+from tests.core._abstract import AbstractTest, start
+
+
+class ListTest(AbstractTest):
+    """Unit-tests for Queue"""
+    pass
+
+
+# Starting point
+if __name__ == 'main':
+    start(False)
